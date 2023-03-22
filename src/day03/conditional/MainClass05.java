e739dd72ba936ce92782eda2ea45bb7daa37251d
ITSC <ITSC@DESKTOP-PMHM7D7>
2023-03-22 21:45:57 +0900

    cc

---
+++ src/day03/conditional/MainClass05.java
@@ -0,0 +1,32 @@
+package day03.conditional;
+
+public class MainClass05 {
+	public static void main(String[] args) {
+		
+		// 01 : 환불신청중
+		// 02 : 환불처리중
+		// 03 : 환불취소
+		// 04 : 계좌이체중
+		// 05 : 환불완료
+		// 01, 02, 04 상태일때 환불 불가!
+		
+		String rtCd = "02";
+		switch(rtCd) {
+		case "01":
+		case "02":
+		case "04":
+			System.out.println("환불 불가!!");
+			System.out.println("환불 상태 코드:" + rtCd);
+			break;
+		}
+		
+		if(rtCd.contentEquals("01") ||
+				rtCd.contentEquals("02") ||
+				rtCd.contentEquals("04") 
+		) {
+			System.out.println("환불불가");
+			System.out.println("환불 상태 코드:" + rtCd);
+		}
+	}
+
+}
