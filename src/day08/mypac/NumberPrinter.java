e6d674d3d78b83df7fb8265b35619cceee5116cf
ITSC <ITSC@DESKTOP-PMHM7D7>
2023-04-03 21:22:26 +0900

    asdasd

---
+++ src/day08/mypac/NumberPrinter.java
@@ -0,0 +1,16 @@
+package day08.mypac;
+
+public class NumberPrinter {
+	
+	public void printNumber() throws InterruptedException {
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i);
+		    Thread.sleep(1000);   // 1초 일시정지n
+			
+			
+		}
+	}
+
+	}
+
+
